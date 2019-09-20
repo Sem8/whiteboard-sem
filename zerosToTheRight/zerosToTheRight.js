6dcc346b11b489cd95c1193c0be61908a50f132e
Sem8 <mssemmi8@gmail.com>
2019-09-20 13:51:35 -0400

    pseudocode for strategy to solve and a code written to solve moving 0s to the right of an array and mutating the original array but strategy and code is faulty

--- zerosToTheRight/zerosToTheRight.js
+++ zerosToTheRight/zerosToTheRight.js
@@ -7,20 +7,49 @@
 6. Return nonZeroNums to get how many non-zero numbers there are.
  */
 
+// const zerosToTheRight = arr => {
+//     let zeroArr = [];
+//     let nonZeroArr = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === 0) {
+//             zeroArr.push(arr[i]);
+//         } else {
+//             nonZeroArr.push(arr[i]);
+//         }
+//     }
+//     let nonZeroNums = nonZeroArr.length;
+//     nonZeroArr.concat(zeroArr);
+//     console.log(nonZeroArr);
+//     return nonZeroNums;
+// }
+
+/* Pseudocode for in place re-arrangement of 0s to mutate the original array.
+1. Initialize an empty array, call it nonZeroArr.
+2. Loop through the input array and make an if statement to check if current array element at current index does not equal zero. If so,
+get that element by storing in a variable called nonZeroNum and push nonZeroNum into nonZeroArr at every iteration.
+3. Outside for loop, get the number of non-0 numbers by getting length of nonZeroArr array and set it to variable called nonZeroAmount
+4. Loop through the nonZeroArr and unshift each element at current index into input arr.
+5. console.log input arr.
+6. Return nonZeroAmount
+ */
+
 const zerosToTheRight = arr => {
-    let zeroArr = [];
     let nonZeroArr = [];
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 0) {
-            zeroArr.push(arr[i]);
-        } else {
-            nonZeroArr.push(arr[i]);
+        if (arr[i] !== 0) {
+            let nonZeroNum = arr.splice(i, 1);
+            nonZeroArr.push(nonZeroNum);
         }
     }
-    let nonZeroNums = nonZeroArr.length;
-    nonZeroArr.concat(zeroArr);
-    return nonZeroNums;
+    let nonZeroAmount = nonZeroArr.length;
+    for (let i = 0; i < nonZeroArr.length; i++) {
+        arr.unshift(nonZeroArr[i]);
+    }
+    console.log(arr);
+    return nonZeroAmount;
+
 }
 
 console.log(zerosToTheRight([0, 3, 1, 0, -2])); // 3
