4435101fefe2a7c69f167d804262bee17f4996ff
Sem8 <mssemmi8@gmail.com>
2019-09-27 23:28:12 -0400

    wrote pseudocode and solution code for optimized solution for merging two package challenge to return indices of 2 numbers from an array of numbers that sum up to a given input integer with first index being the higher index

--- merginTwoPackages/mergingTwoPackages.js
+++ merginTwoPackages/mergingTwoPackages.js
@@ -18,46 +18,71 @@ statement, check if array length is equal to 2 and array element at 0th index pl
 return [1, 0]  
  */
 
-// My own solution: time complexity is ~ O(2n)
-const getIndicesOfItemWeights = (arr, targetWeight) => {
+// My own solution: time complexity is ~ O(3n) and space complexity of O(n);
+// const getIndicesOfItemWeights = (arr, targetWeight) => {
+//   let hashTable = {};
+
+//   if (arr.length < 2) {
+//     return [];
+//   };
+
+//   if (arr.length == 2 && arr[0] + arr[1] === targetWeight) {
+//     return [1, 0];
+//   };
+
+//   for (let i = 0; i < arr.length; i++) {
+//     let difference = targetWeight - arr[i];
+
+//     if (!hashTable[difference]) {
+//       hashTable[difference] = arr[i];
+//     }
+//   };
+//   // console.log(hashTable);
+//   for (arrItem in hashTable) {
+//     if (arr.indexOf(parseInt(arrItem)) !== -1) {
+//       // console.log(hashTable[arrItem], arrItem);
+//       if (arr.indexOf(hashTable[arrItem]) >= arr.indexOf(parseInt(arrItem))) {
+//         return [arr.indexOf(hashTable[arrItem]), arr.indexOf(parseInt(arrItem))];
+//       } else {
+//         return [arr.indexOf(parseInt(arrItem)), arr.indexOf(hashTable[arrItem])];
+//       };
+//     };
+//   };
+// };
+
+// Optimized solution: Time complexity- ~O(n) ; space complexity- ~O(n) at worst, O( < n) at average:
+/*Pseudocode:
+1. Initialize an empty object call it hashTable
+2. Loop through the array, with for loop and initialize a variable called currentElement and set it equal to current array 
+element at current index
+3. Initialize a variable called differenceIndex and set it to be as a key in the hashTable object, where this key difference 
+will be input target number minus current element at current index.
+4. Make an if statement, check if the differenceIndex key in hashTable object has a real value. If so, return in an array, 
+the current array index and the value of the difference key from the hashTable object. 
+5. Else (if hashTable object does not have a real value for the difference key, since initially it won't), set the current array
+element at current index as a key and it's value to be the current array index. Loop will repeat itself until a difference is 
+found as an array element, then it'll return the later array element index and the current difference between current 
+element and input target weight and that difference's index in the array.
+6. Outside for loop, return empty array if no 2 numbers in array equals the input target weight number.
+ */
+
+const getIndicesOfItemWeights = (arr, inputWeight) => {
   let hashTable = {};
 
-  if (arr.length < 2) {
-    return [];
-  }
-
-  if (arr.length == 2 && arr[0] + arr[1] === targetWeight) {
-    return [1, 0];
-  }
-
   for (let i = 0; i < arr.length; i++) {
-    let difference = targetWeight - arr[i];
+    let currentElement = arr[i];
+    let differenceIndex = hashTable[inputWeight - currentElement];
 
-    if (!hashTable[difference]) {
-      hashTable[difference] = arr[i];
+    if (differenceIndex !== undefined) {
+      return [i, differenceIndex];
+    } else {
+      hashTable[currentElement] = i
     }
-  }
-  // console.log(hashTable);
-  for (arrItem in hashTable) {
-    if (
-      arr.indexOf(hashTable[arrItem]) !== -1 &&
-      arr.indexOf(parseInt(arrItem)) !== -1
-    ) {
-      // console.log(hashTable[arrItem], arrItem);
-      if (arr.indexOf(hashTable[arrItem]) >= arr.indexOf(parseInt(arrItem))) {
-        return [
-          arr.indexOf(hashTable[arrItem]),
-          arr.indexOf(parseInt(arrItem))
-        ];
-      } else {
-        return [
-          arr.indexOf(parseInt(arrItem)),
-          arr.indexOf(hashTable[arrItem])
-        ];
-      }
-    }
-  }
-};
+  };
+
+  return [];
+}
+
 
 console.log(getIndicesOfItemWeights([4, 6, 10, 15, 16], 21)); // [3 , 1]
 console.log(getIndicesOfItemWeights([4, 4], 8)); // [1 , 0]
